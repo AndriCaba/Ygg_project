15ec935e57ae1bae91a45bf4af4733396b34cfe1
FUFU66 <148789740+Sampalok01@users.noreply.github.com>
2023-11-18 15:40:58 +0800

    Scene Transitions Animations (YGG CONTEST))

---
+++ Assets/SCRIPTS/LevelLoader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewSceneLoader : MonoBehaviour
+{
+
+    public Animator transition1, transition2;
+    public float transitionTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadPreviousScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition1.SetTrigger("Start");
+        transition2.SetTrigger("Start2");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
